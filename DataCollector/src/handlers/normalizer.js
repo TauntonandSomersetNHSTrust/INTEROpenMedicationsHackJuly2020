2c759fcebfc5b3accfb760d73d0f8b54574ab9a1
Mark Hunt <Mark.Hunt@tst.nhs.uk>
2020-07-15 12:19:18 +0100

    normalizer

--- DataCollector/src/handlers/normalizer.js
+++ DataCollector/src/handlers/normalizer.js
@@ -28,9 +28,9 @@ exports.getSummaryFromGPCStructured = (GPStructured) => {
 						entries :[]
 					}
 					
-					console.log(tidylist);
-					console.log('');
-					collection[GPStructured.entry[a].resource.resourceType].push(tidylist);
+					// console.log(tidylist);
+					// console.log('');
+					// collection[GPStructured.entry[a].resource.resourceType].push(tidylist);
 					
 					if(GPStructured.entry[a].resource.entry && GPStructured.entry[a].resource.entry.length > 0) {
 						for (let x = 0; x < GPStructured.entry[a].resource.entry.length; x++ ){
@@ -41,8 +41,10 @@ exports.getSummaryFromGPCStructured = (GPStructured) => {
 								eId = eId.replace('#','');
 							}
 							console.log(eId);
+							console.log("searching.....");
 							let found = false;
-							if(GPStructured.entry[a].resource.entry[x].contained && GPStructured.entry[a].resource.entry[x].contained.length > 0) { // Are We Self Contained?
+							console.log(GPStructured.entry[a].resource);
+							if(GPStructured.entry[a].resource.contained && GPStructured.entry[a].resource.contained.length > 0) { // Are We Self Contained?
 								for (let b = 0; b < GPStructured.entry[a].resource.entry[x].contained.length; b++) {
 									console.log(GPStructured.entry[a].resource.entry[x].contained[b]);
 									if(GPStructured.entry[a].resource.entry[x].contained[b] && GPStructured.entry[a].resource.entry[x].contained[b].toLowerCase() === eId.toLowerCase()) {
@@ -51,7 +53,7 @@ exports.getSummaryFromGPCStructured = (GPStructured) => {
 									}
 								}
 							}
-							
+							console.log("more searching.....");
 							if(found === false && GPStructured.entry.length > 0){
 								const type = eId.split('/')[0] ? eId.split('/')[0].replace('/') : eId.split('/')[0];
 								const id = eId.split('/')[1] ? eId.split('/')[1].replace('/') : eId.split('/')[0];
