cbfa4fe07dc10830ac605aa5cb5045511f297aa6
Mark Hunt <Mark.Hunt@tst.nhs.uk>
2020-07-15 08:45:52 +0100

    Normalizer updates

--- DataCollector/src/handlers/normalizer.js
+++ DataCollector/src/handlers/normalizer.js
@@ -22,39 +22,49 @@ exports.getSummaryFromGPCStructured = (GPStructured) => {
 				//ollection[GPStructured.entry[i].resource.resourceType].push(GPStructured.entry[i].resource);
 				if(GPStructured.entry[a].resource.resourceType.toLowerCase() === 'list'){
 					let tidylist = {
-						title : collection[GPStructured.entry[a].resource.resourceType].push(GPStructured.entry[a].resource.title),
-						status : collection[GPStructured.entry[a].resource.resourceType].push(GPStructured.entry[a].resource.status),
-						code : collection[GPStructured.entry[a].resource.resourceType].push(GPStructured.entry[a].resource.code),
+						title : GPStructured.entry[a].resource.title,
+						status : GPStructured.entry[a].resource.status,
+						code : GPStructured.entry[a].resource.code,
 						entries :[]
 					}
+					
+					console.log(tidylist);
+					console.log('');
+					collection[GPStructured.entry[a].resource.resourceType].push(tidylist);
+					
 					if(GPStructured.entry[a].resource.entry && GPStructured.entry[a].resource.entry.length > 0) {
-						for (entry in collection[GPStructured.entry[a].resource.entry]){
+						for (let x = 0; x < GPStructured.entry[a].resource.entry.length; x++ ){
+							console.log(GPStructured.entry[a].resource.entry[x]);
 							let shinyEntry = {};
-							const eId = entry.item.reference;
+							let eId = GPStructured.entry[a].resource.entry[x].item.reference;
 							if(eId.startsWith('#') && eId.indexOf('/') == -1 ){
 								eId = eId.replace('#','');
 							}
 							console.log(eId);
-							if(entry.contained && entry.contained.length > 0) { // Are We Self Contained?
-								found = false;
-								for (let b = 0; b < entry.contained.length; b++) {
-									console.log(entry.contained[b]);
-									if(entry.contained[b] && entry.contained[b].toLowerCase() === eId.toLowerCase()) {
-										shinyEntry = entry.contained[b];
+							let found = false;
+							if(GPStructured.entry[a].resource.entry[x].contained && GPStructured.entry[a].resource.entry[x].contained.length > 0) { // Are We Self Contained?
+								for (let b = 0; b < GPStructured.entry[a].resource.entry[x].contained.length; b++) {
+									console.log(GPStructured.entry[a].resource.entry[x].contained[b]);
+									if(GPStructured.entry[a].resource.entry[x].contained[b] && GPStructured.entry[a].resource.entry[x].contained[b].toLowerCase() === eId.toLowerCase()) {
+										shinyEntry = GPStructured.entry[a].resource.entry[x].contained[b];
 										found = true;
 									}
 								}
 							}
 							
 							if(found === false && GPStructured.entry.length > 0){
-								const type = eId.split('/')[0].replace('/');
-								const id = eId.split('/')[1].replace('/');
+								const type = eId.split('/')[0] ? eId.split('/')[0].replace('/') : eId.split('/')[0];
+								const id = eId.split('/')[1] ? eId.split('/')[1].replace('/') : eId.split('/')[0];
 								for (sEntry in GPStructured.entry) {
 									if(sEntry.resourceType && sEntry.resourceType.toLowerCase() === type.toLowerCase() && sEntry.id && sEntry.id == id ) {
 										shinyEntry = sEntry;
 										found=true;
 									}
 								}
+							}
+							
+							if(found === false) {
+								let shinyEntry = GPStructured.entry[a].resource.entry[x].item;
 							}
 						
 							tidylist.entries.push(shinyEntry);
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# INTEROpenMedicationsHackJuly2020
+# INTEROpen Medications Hackathon July 2020
+
+#### GP Connect Endpoint
+
+https://orange.testlab.nhs.uk/
+
+https://orange.testlab.nhs.uk/opentest.html
 
 
+
+#### Test Resources
 
 https://fhirdevdemo.tst.nhs.uk/HackJuly2020/gprecord-fake.json
 
@@ -8,3 +16,12 @@ https://fhirdevdemo.tst.nhs.uk/HackJuly2020/acute-meds.html
 
 https://fhirdevdemo.tst.nhs.uk/HackJuly2020/current-meds.html
 
+
+
+#### Published Node Service
+
+Base URL: https://cloudservices.mhtechnology.co.uk/gpc
+
+Example
+
+https://cloudservices.mhtechnology.co.uk/gpc/gpc/structured/9658218873
