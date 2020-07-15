482b00ec4feb32ef2a822d184e495a442211bb38
Haydn Williams <Haydn.Williams@tst.nhs.uk>
2020-07-15 06:46:46 +0100

    Tidying up the new scopes code, removing comments and console logging.

--- DataCollector/src/handlers/gpc.js
+++ DataCollector/src/handlers/gpc.js
@@ -5,6 +5,11 @@ const request = require('request');
 
 const logger = require('./logger');
 
+const SCOPES = {
+	organization: 'organization/*.read',
+	patient: 'patient/*.read'
+}
+
 exports.getJWT = (scope) => {
 	const requesting_organization_ODS_Code = "A11111";
 	// Construct the JWT token for the request
@@ -21,6 +26,11 @@ exports.getJWT = (scope) => {
 	payload.exp = jwtExpiryTime;
 	payload.iat = jwtCreationTime;
 	payload.requesting_organization.identifier[0].value = requesting_organization_ODS_Code;
+
+	if(!scope || scope.length === 0) {
+		scope = SCOPES.organization;
+	}
+
 	payload.requested_scope = scope;
 
 	// Encode the JWT data into the base64url encoded string
@@ -52,46 +62,9 @@ exports.getStructuredMedicalRecord = (nhsNo) => {
 		body.parameter[0].valueIdentifier.value = '' + nhsNo;
 
 		const qs = require('querystring');
-		
-		console.log([process.env.GPConnect_1_Base, 'Patient/$gpc.getstructuredrecord'].join('/'));
-		/*
-		var requestData = {
-		  resourceType: "Parameters",
-		  parameter: [
-		  {
-			name : "patientNHSNumber",
-			valueIdentifier : {
-				system : "https://fhir.nhs.uk/Id/nhs-number",
-				value : nhsNo
-			}
-		  },
-		  {
-			name : "includeAllergies",
-			part : [{
-				name: "includeResolvedAllergies",
-				valueBoolean: true
-			}]
-		  },
-		  {
-			name : "includeMedication",
-			part : [{
-				name: "includePrescriptionIssues",
-				valueBoolean: true
-			},
-			{
-				name: "medicationSearchFromDate",
-				valueBoolean: true
-			}]
-		  }
-		  ]
-		};
-
-		*/
-		console.log(body);
 		const options = {
 			method: 'POST',
-			url: 'https://orange.testlab.nhs.uk/gpconnect-demonstrator/v1/fhir/Patient/$gpc.getstructuredrecord',
-			//json: true,
+			url: [process.env.GPConnect_1_Base, process.env.GPConnect_1_structured].join('/'),
 			body: JSON.stringify(body),
 			headers: {
 				'Content-Type': process.env.GPConnect_Accept,
@@ -100,11 +73,9 @@ exports.getStructuredMedicalRecord = (nhsNo) => {
 				'Ssp-To': process.env.GPConnect_SSPTo,
 				'Ssp-InteractionID': process.env.GPConnect_SSPInteractionID_Structured,
 				'Ssp-TraceID': uuid(),
-				'Authorization': 'Bearer ' + this.getJWT('patient/*.read')
+				'Authorization': 'Bearer ' + this.getJWT(SCOPES.patient)
 			}
 		};
-
-		console.log(options);
 
 		request(options, (error, response, body) => {
 			if (error) {
@@ -117,11 +88,8 @@ exports.getStructuredMedicalRecord = (nhsNo) => {
 
 exports.getStructuredMedicalRecordDemo = (nhsNo) => {
 	return new Promise((resolve, reject) => {
-
 		const body =  require('./../secure/patients/'+nhsNo+'.json');
-
 		return resolve(body);
-
 	});
 };
 
@@ -136,7 +104,7 @@ exports.getPatientByNHSNo = (nhsNo) => {
 				'Ssp-To': process.env.GPConnect_SSPTo,
 				'Ssp-InteractionID': process.env.GPConnect_SSPInteractionID_Patient,
 				'Ssp-TraceID': uuid(),
-				'Authorization': 'Bearer ' + this.getJWT('organization/*.read')
+				'Authorization': 'Bearer ' + this.getJWT(SCOPES.patient)
 			}
 		};
 
@@ -162,7 +130,7 @@ exports.metadata = () => {
 				'Ssp-To': process.env.GPConnect_SSPTo,
 				'Ssp-InteractionID': process.env.GPConnect_SSPInteractionID_Metadata,
 				'Ssp-TraceID': uuid(),
-				'Authorization': 'Bearer ' + this.getJWT('organization/*.read')
+				'Authorization': 'Bearer ' + this.getJWT(SCOPES.organization)
 			}
 		};
 
